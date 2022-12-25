8206954da56d0d7ea50c627d07a84abb0b19498c
Anirban-45 <azadarannya@gmail.com>
2022-12-25 11:24:18 +0600

    build version created

---
+++ build/precache-manifest.22a9666d9727f2a7e096bd7d703dd84b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ccb91b88a0657e23eb7cf45af7522da",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6637c645f9ba51c32e76",
+    "url": "/static/css/main.5a926c7a.chunk.css"
+  },
+  {
+    "revision": "b530a833a2a1987bb1f2",
+    "url": "/static/js/2.49ce2399.chunk.js"
+  },
+  {
+    "revision": "6637c645f9ba51c32e76",
+    "url": "/static/js/main.e4800201.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
